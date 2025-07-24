83235ae4eb14039e3e03034cf76d2e6061ddbc48
pramindu123 <pramindukumaranayake999@gmail.com>
2025-07-24 16:03:26 +0530

    commit

--- src/components/Navbar.tsx
+++ src/components/Navbar.tsx
@@ -85,12 +85,12 @@ const Navbar = () => {
 
       {/* Mobile menu sliding panel */}
       <div 
-        className={`fixed top-0 right-0 h-full w-64 bg-white shadow-lg transform transition-transform duration-300 ease-in-out z-50 sm:hidden ${
+        className={`fixed top-0 right-0 h-screen w-64 bg-white shadow-lg transform transition-transform duration-300 ease-in-out z-50 sm:hidden ${
           isOpen ? 'translate-x-0' : 'translate-x-full'
         }`}
         style={{ backgroundColor: 'white' }}
       >
-        <div className="flex flex-col h-full bg-white">
+        <div className="flex flex-col h-screen bg-white">
           <div className="flex justify-end p-4 bg-white">
             <button
               onClick={() => setIsOpen(false)}
