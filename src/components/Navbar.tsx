0d7450ef2b2639670f76df58920eea3add4a2968
pramindu123 <pramindukumaranayake999@gmail.com>
2025-07-24 15:55:37 +0530

    commit

--- src/components/Navbar.tsx
+++ src/components/Navbar.tsx
@@ -88,6 +88,7 @@ const Navbar = () => {
         className={`fixed top-0 right-0 h-full w-64 bg-white shadow-lg transform transition-transform duration-300 ease-in-out z-50 sm:hidden ${
           isOpen ? 'translate-x-0' : 'translate-x-full'
         }`}
+        style={{ backgroundColor: 'white' }}
       >
         <div className="flex flex-col h-full">
           <div className="flex justify-end p-4">
