21f9b271d20ab9db618e256afe5c058c268c5401
pramindu123 <pramindukumaranayake999@gmail.com>
2025-07-26 19:20:22 +0530

    commit

--- src/components/Navbar.tsx
+++ src/components/Navbar.tsx
@@ -85,9 +85,10 @@ const Navbar = () => {
 
       {/* Mobile menu sliding panel */}
       <div 
-        className={`fixed top-0 right-0 h-full w-64 bg-white shadow-lg transform transition-transform duration-300 ease-in-out z-50 sm:hidden ${
+        className={`fixed top-0 right-0 h-full w-64 bg-white shadow-lg transition-transform duration-300 ease-in-out z-50 sm:hidden ${
           isOpen ? 'translate-x-0' : 'translate-x-full'
         }`}
+        style={{ backgroundColor: "#fff" }} // Ensures solid white background
       >
         <div className="flex flex-col h-full">
           <div className="flex justify-end p-4">
