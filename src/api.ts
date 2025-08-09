b1015cae96d82484d3c7b2ce0f978f14edf87e01
pramindu123 <pramindukumaranayake999@gmail.com>
2025-08-09 21:46:40 +0530

    commit

--- src/api.ts
+++ src/api.ts
@@ -1,2 +1,2 @@
 // API Configuration
-export const API_BASE_URL = 'http://hazardx-gyh7bwguapcjdccm.southindia-01.azurewebsites.net';
+export const API_BASE_URL = 'https://hazardx-gyh7bwguapcjdccm.southindia-01.azurewebsites.net';
